a41676652c290184dcc27ac9dea8a68438b3f663
nikhilshaw3 <69413813+nikhilshaw3@users.noreply.github.com>
2020-08-14 13:19:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
   weight = random(30,52);
   thickness = random(22,83);
   
-  bullet = createSprite(50, 200, 50, 50);
+  bullet = createSprite(50, 200, 50, 5);
   bullet.velocityX = speed;
   bullet.shapeColor = "white";
   
